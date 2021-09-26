c777b2f83d765558a03d2bd912f129afc30f892f
Nymph <QuantNymph@gmail.com>
2021-09-26 07:20:00 -0500

    v1

--- arbibots_mints.py
+++ arbibots_mints.py
@@ -1,19 +1,24 @@
 from web3 import Web3
 from configparser import ConfigParser
+from ratelimit import sleep_and_retry, limits
 
 import asyncio
 import json
 import websockets
 import redis
 
-async def newPendingTransactionFilter(uri):
+@sleep_and_retry
+@limits(calls=150, period=1)
+async def newBlockFilter(uri):
     async with websockets.connect(uri) as websocket:
-        request_data = {"jsonrpc":"2.0","id":"1","method":"eth_newPendingTransactionFilter","params":[],"id": 1}
+        request_data = {"jsonrpc":"2.0","id":"1","method":"eth_newBlockFilter","params":[],"id": 1}
         await websocket.send(json.dumps(request_data))
 
         result = await websocket.recv()
         return json.loads(result)['result']
 
+@sleep_and_retry
+@limits(calls=150, period=1)
 async def getFilterChanges(uri, filter_hash):
     async with websockets.connect(uri) as websocket:
         request_data = {"jsonrpc":"2.0","id":"1","method":"eth_getFilterChanges","params":[filter_hash], "id": 1}
@@ -21,7 +26,9 @@ async def getFilterChanges(uri, filter_hash):
 
         result = await websocket.recv()
         return json.loads(result)['result']
-    
+
+@sleep_and_retry
+@limits(calls=150, period=1) 
 async def getTransactionByHash(uri, tx_hash):
     async with websockets.connect(uri) as websocket:
         request_data = {"jsonrpc":"2.0","id":"1","method":"eth_getTransactionByHash","params":[tx_hash], "id": 1}
@@ -30,26 +37,59 @@ async def getTransactionByHash(uri, tx_hash):
         result = await websocket.recv()
         return json.loads(result)['result']    
 
-if __name__ == "__main__":
-    cfg = ConfigParser().read('config.cfg')
+@sleep_and_retry
+@limits(calls=150, period=1)
+async def getBlockTransactionsByHash(uri, block_hash):
+    async with websockets.connect(uri) as websocket:
+        request_data = {"jsonrpc":"2.0","id":"1","method":"eth_getBlockByHash","params":[block_hash, True], "id": 1}
+        await websocket.send(json.dumps(request_data))
 
-    filter_hash = asyncio.get_event_loop().run_until_complete(newPendingTransactionFilter(cfg['NodeURLs']['arbitrum_wss']))
+        result = await websocket.recv()
+        return json.loads(result)['result']['transactions']
+
+def is_arbibots_mint(tx):
+    if tx == None:
+        return False
+    if (tx['to'] == '0xc1fCf330b4B4C773fA7e6835f681E8F798E9eBff') & ('data' in tx.keys()):
+        if (tx['data'][0:10] == '0x1249c58b'):
+            return True
+
+def mints_in_block(txs):
+    bools = [is_arbibots_mint(tx) for tx in txs]
+
+    return bools.count(True)
+
+def mints_in_blocks(blocks):
+    counts = [mints_in_block(txs) for txs in blocks]
+
+    return sum(counts)
+
+if __name__ == "__main__":
+    cfg = ConfigParser()
+    cfg.read('config.cfg')
+
+    filter_hash = asyncio.get_event_loop().run_until_complete(newBlockFilter(cfg['NodeURLs']['arbitrum_wss']))
     mempool = asyncio.get_event_loop().run_until_complete(getFilterChanges(cfg['NodeURLs']['arbitrum_wss'], filter_hash))
     
     contract_address = "0xc1fCf330b4B4C773fA7e6835f681E8F798E9eBff"
 
     r0 = redis.StrictRedis(host='localhost', port=6379, password=cfg['PASSWORDS']['redis'], db=0)
-
-    id = 0
+    
     while True:
-        mempool_changes = asyncio.get_event_loop().run_until_complete(getFilterChanges(cfg['NodeURLs']['arbitrum_wss'], filter_hash))
-        
-        if len(mempool_changes) > 0:
-            txs = [asyncio.get_event_loop().run_until_complete(getTransactionByHash(cfg['NodeURLs']['arbitrum_wss'], tx)) for tx in mempool_changes]
-            
-            for tx in txs:
-                if tx == None:
-                    continue
-                if (tx['to'] == contract_address) & (tx['data'][0:10] == '0x1249c58b'):
-                    r0.set(id, Web3.toInt(hexstr=tx['gasPrice']))
-                    id += 1
+        try:
+            filter_changes = asyncio.get_event_loop().run_until_complete(getFilterChanges(cfg['NodeURLs']['arbitrum_wss'], filter_hash))
+        except :
+            filter_hash = asyncio.get_event_loop().run_until_complete(newBlockFilter(cfg['NodeURLs']['arbitrum_wss']))
+            continue
+                
+        if len(filter_changes) > 0:
+            for change in filter_changes:
+                blocks = [asyncio.get_event_loop().run_until_complete(getBlockTransactionsByHash(cfg['NodeURLs']['arbitrum_wss'], tx)) for tx in filter_changes]
+
+                inc = mints_in_blocks(blocks)
+                
+                if inc > 0:
+                    current = int(r0.get(0))
+                    if current >= 25:
+                        r0.set(0, 0)
+                    r0.set(0, current + inc)
--- arbibotter.py
+++ arbibotter.py
@@ -1,20 +1,32 @@
 from configparser import ConfigParser
 from web3 import Web3
+from web3.auto import w3
+
 from ratelimit import limits, sleep_and_retry
 
 import json
 import numpy as np
 
 import sys
-import redis
+
 import time
-import math
+import redis
 
-def getHighestGas(redis_password):
-    r = redis.StrictRedis(host='localhost', port=6379, password=redis_password, db=0)
-    gasPrices = np.array([int(r.get(key)) for key in r.scan_iter()])
+def offchain_mintPrice(secondsSinceAuctionStart):    
+    if (secondsSinceAuctionStart < 18000):
+        return 2 * 10**19 - (2 * 10**19 * secondsSinceAuctionStart) / 18305
+    elif (secondsSinceAuctionStart < 21492):
+        return 2 * 10**18 - (2 * 10**18 * secondsSinceAuctionStart) / 21600
+    else:
+        return 10**16
 
-    return int(math.floor(max(gasPrices)) / (10 ** 7))
+def offchain_secondsSinceAuctionStart(buy_price):
+    if buy_price > offchain_mintPrice(18000):
+        return 18305 - (3661 * buy_price)/4000000000000000000
+    elif buy_price > offchain_mintPrice(21492):
+        return 21600 - (27 * buy_price)/2500000000000000
+    else:
+        return 21492
 
 @sleep_and_retry
 @limits(150, period=1)
@@ -36,8 +48,15 @@ def wait_for_tx(instance, hash):
 def send_tx(instance, signed_tx):
     instance.eth.send_raw_transaction(signed_tx.rawTransaction)
 
-def mint_at(personal_address, privatekey, buy_price, qty, gwei):
-    node = Web3(Web3.WebsocketProvider(cfg['NodeURLs']['arbitrum_wss']))
+def get_num_mints(redis_instance):
+    redis_instance.get(0)
+
+def mint_at(personal_address, privatekey, node, buy_price, qty, gwei, panic_point=0, max_price=None):
+    if panic_point == 0:        
+        max_price = buy_price
+    else:
+        r0 = redis.StrictRedis(host='localhost', port=6379, password=cfg['PASSWORDS']['redis'], db=0)
+        max_price = max_price
 
     abi = json.loads('[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"price","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"MAX_BOTS","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"TOTAL_REWARD_POOLS","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"bodies","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"eyes","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"headgears","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"heads","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"mintPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"mouths","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"palettes","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"redeem","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"redeemable","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"redeemed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"rewardPools","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"seeds","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalRewards","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
 
@@ -45,18 +64,29 @@ def mint_at(personal_address, privatekey, buy_price, qty, gwei):
     contract =  node.eth.contract(address=contract_address, abi=abi)
 
     gas_price = Web3.toWei(gwei, 'gwei')
-    current_nonce = node.eth.get_transaction_count(personal_address)
-    txs = [{'chainId': 42161, 'from': personal_address, 'value': Web3.toWei(buy_price, 'ether'), 'gasPrice': gas_price, 'nonce': nonce} for nonce in range(current_nonce, current_nonce+int(qty)+1)]
-    
+
+    start = time.monotonic()
     mint_price = np.float64(Web3.fromWei(contract.functions.mintPrice().call(), 'ether'))
+    current_seconds = offchain_secondsSinceAuctionStart(mint_price)
 
-    while mint_price > (buy_price + 1):
-        print(mint_price)
-        time.sleep(10)
-        mint_price = np.float64(Web3.fromWei(contract.functions.mintPrice().call(), 'ether'))
+    if panic_point == 0:
+        target_seconds = offchain_secondsSinceAuctionStart(buy_price)
+    else:
+        target_seconds = offchain_secondsSinceAuctionStart(max_price)
 
-    for tx in txs:
-        signed_tx = node.eth.sign_transaction(contract.functions.mint().buildTransaction(tx), privatekey=privatekey)        
+    end = time.monotonic()
+    time.sleep(target_seconds - current_seconds - (end - start) - 60)
+
+    if panic_point > 0:
+        while np.float64(Web3.fromWei(contract.functions.mintPrice().call(), 'ether')) > np.float64(buy_price):
+            n = get_num_mints(r0)
+            if n > panic_point:
+                break        
+            time.sleep(1)
+
+    for nonce in range(node.eth.get_transaction_count(personal_address), qty+1):
+        tx = {'chainId': 42161, 'from': personal_address, 'value': str(Web3.toHex(Web3.toWei(np.float64(Web3.fromWei(contract.functions.mintPrice().call(), 'ether')), 'ether'))), 'gasPrice': str(Web3.toHex(Web3.toWei(gas_price, 'gwei'))), 'nonce': nonce}
+        signed_tx = w3.eth.account.sign_transaction(contract.functions.mint().buildTransaction(tx), private_key=privatekey)        
         hash = node.toHex(node.keccak(signed_tx.rawTransaction))
         
         send_tx(node, signed_tx)
@@ -68,4 +98,11 @@ cfg.read('config.cfg')
 privatekey = cfg["IDENTITY"]['private_key']
 personal_address = cfg["IDENTITY"]['personal_address']
 
-mint_at(personal_address, privatekey, np.float64(sys.argv[1]), int(sys.argv[2]), math.round(getHighestGas(cfg['PASSWORDS']['redis']) * 1.05, 2))
+node = Web3(Web3.WebsocketProvider(cfg['NodeURLs']['arbitrum_wss']))
+
+if len(sys.argv) == 3:
+    mint_at(personal_address, privatekey, node, np.float64(sys.argv[1]), int(sys.argv[2]), 3)
+elif len(sys.argv) == 5:
+    mint_at(personal_address, privatekey, node, np.float64(sys.argv[1]), int(sys.argv[2]), 3, sys.argv[3], sys.argv[4])
+else:
+    print('arguments must be MINT_PRICE QUANTITY and optionally be followed by PANIC_POINT MAX_PRICE')
